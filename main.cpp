060a576a675dc0cd71d152b017f3ceb06142fc10
vodoanhoanglong <67463870+vodoanhoanglong@users.noreply.github.com>
2020-12-04 13:06:22 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 
 int main()
 {
-	BTL::Game(SCREEN_WIDTH, SCREEN_HEIGHT, "Flappy Bird"); // class Game nằm trong phạm vi project Sonar, truyền tham số vào
+	BTL::Game(SCREEN_WIDTH, SCREEN_HEIGHT, "Flappy Bird"); // class Game nằm trong phạm vi project BTL, truyền tham số vào
 
 	return EXIT_SUCCESS; // tương đương return 0
 }
